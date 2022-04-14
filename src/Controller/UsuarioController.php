f3d0c2cae66aac00365ddcfe91d331c8edca1a53
Anderson-Makino <anddersonful@gmail.com>
2022-04-14 17:27:35 -0300

    edit user hasing password

--- src/Controller/UsuarioController.php
+++ src/Controller/UsuarioController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 #[Route('/usuario')]
 class UsuarioController extends AbstractController
@@ -50,12 +51,18 @@ class UsuarioController extends AbstractController
     }
 
     #[Route('/{id}/edit', name: 'app_usuario_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Usuario $usuario, UsuarioRepository $usuarioRepository): Response
+    public function edit(Request $request, Usuario $usuario, UsuarioRepository $usuarioRepository, UserPasswordHasherInterface $passwordHasher): Response
     {
         $form = $this->createForm(UsuarioType::class, $usuario);
         $form->handleRequest($request);
+        $plaintextPassword = $usuario->getPassword();
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $hashedPassword = $passwordHasher->hashPassword(
+                $usuario,
+                $plaintextPassword
+            );
+            $usuario->setPassword($hashedPassword);
             $usuarioRepository->add($usuario);
             return $this->redirectToRoute('app_usuario_index', [], Response::HTTP_SEE_OTHER);
         }
